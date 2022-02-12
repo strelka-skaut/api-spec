1986deea184a5653c3caa2b9b363eba932fc4b3e
MrVoltz <uzivatelmp@gmail.com>
2022-02-12 21:22:33 +0100

    1.0.14: Add galleries

--- js-api-client/src/index.ts
+++ js-api-client/src/index.ts
@@ -5,6 +5,8 @@ import { Uuid } from "./common_pb";
 export * as Common from "./common_pb";
 export * as Pages from "./pages_pb";
 export * as PagesClient from "./PagesServiceClientPb";
+export * as Media from "./media_pb";
+export * as MediaClient from "./MediaServiceClientPb";
 
 export function makeUuid(uuidStr: string): Uuid {
 	let uuid = new Uuid;
--- js-api-spec/src/index.ts
+++ js-api-spec/src/index.ts
@@ -1,2 +1,3 @@
 export * as Common from "./common_pb";
 export * as Pages from "./pages_pb";
+export * as Media from "./media_pb";
