5b68814107b85eefa338789e89c93be6373e3131
MrVoltz <uzivatelmp@gmail.com>
2022-02-14 23:51:45 +0100

    1.0.19: Sites, GetXBySlug, UpdateX

--- js-api-client/src/index.ts
+++ js-api-client/src/index.ts
@@ -3,6 +3,8 @@ import "./xhr2-loader";
 import { Uuid } from "./common_pb";
 
 export * as Common from "./common_pb";
+export * as Sites from "./sites_pb";
+export * as SitesClient from "./SitesServiceClientPb";
 export * as Pages from "./pages_pb";
 export * as PagesClient from "./PagesServiceClientPb";
 export * as Media from "./media_pb";
--- js-api-spec/src/index.ts
+++ js-api-spec/src/index.ts
@@ -1,3 +1,4 @@
 export * as Common from "./common_pb";
+export * as Sites from "./sites_pb";
 export * as Pages from "./pages_pb";
 export * as Media from "./media_pb";
